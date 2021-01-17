b35b0a71dc7ff10bb1aaa050a5191b43369d59c1
unknown <jpendy@gmail.com>
2021-01-17 12:38:39 -0600

    exercise completed

---
+++ app.js
@@ -0,0 +1,15 @@
+// ES5 Global Constants
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+//ES2015 Global Constants
+const PI = 3.14;
+
+//Quiz
+//What is the difference between var and let? = Var is function scoped, let is block scoped.  Var can be redeclared and let cannot.  Var is hoisted
+
+//What is the difference between var and cosnt? = var is functio scoped, const is block scoped.  Var can be reassigned and redeclared and const cannot do either. Var is hoisted
+
+//What is the difference between let and const? = let can be reassigned and const cannot.
+
+//hoisting is when the variable is declared at the top of the scope it is defined in, prior to its initialization.
